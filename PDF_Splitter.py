7c2f18f4b08d7197aa53a91cc70030649a35cae0
BigWolf <119083621+imrsaleh@users.noreply.github.com>
2024-01-17 01:57:18 +0300

    PDF_Splitter.py

--- PDF_Splitter.py
+++ PDF_Splitter.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import sys
+import os 
 
 file_name = sys.argv[1]
 pages_per_file = int(sys.argv[2])
@@ -11,6 +12,12 @@ total_pages = pdf_reader.getNumPages()
 
 num_files = (total_pages + pages_per_file - 1) // pages_per_file
 
+base_name = os.path.splitext(os.path.basename(file_name))[0]
+
+output_dir = base_name
+if not os.path.exists(output_dir): 
+    os.mkdir(output_dir) 
+
 for i in range(num_files):
     pdf_writer = PyPDF2.PdfFileWriter()
 
@@ -20,7 +27,11 @@ for i in range(num_files):
     for j in range(start_page, end_page):
         pdf_writer.addPage(pdf_reader.getPage(j))
 
-    target_pdf = open(f"file{i + 1}.pdf", "wb")
+    file_name = f"{start_page + 1}-{end_page}.pdf"
+
+    target_path = os.path.join(output_dir, file_name)
+
+    target_pdf = open(target_path, "wb")
 
     pdf_writer.write(target_pdf)
 
